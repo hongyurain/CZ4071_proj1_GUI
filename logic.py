99c1c1dcfb13315dbfd4a21c7a04b800ddd9ebb9
hongyurain <44743777+hongyurain@users.noreply.github.com>
2021-02-24 21:54:10 +0800

    checkbox and drop down list for year

--- logic.py
+++ logic.py
@@ -4,9 +4,10 @@ from PyQt5.QtGui import *
 from PyQt5.QtCore import *
 # from PyQt5.QtWebEngineWidgets import *
 import pandas as pd
+from preprocessing import generate_graph
 
-from windows import Ui_MainWindow, Ui_Dialog
-from checkbox_2 import checkbox_Dialog
+from windows import Ui_MainWindow, Ui_Dialog,checkbox_Dialog
+#from checkbox_2 import checkbox_Dialog
 
 class MyDialog(QDialog):
     def __init__(self):
@@ -18,6 +19,15 @@ class MyDialog(QDialog):
         self.myWin = MyWindow()
         self.myWin.show()
 
+class CheckBox(QDialog):
+    def __init__(self):
+        super().__init__()
+        self.ui = checkbox_Dialog()
+        self.ui.setupUi(self)
+    def myWindow(self):
+        self.hide()
+        self.myWin = MyWindow()
+        self.myWin.show()
 class MyWindow(QMainWindow):
     def __init__(self):
         super().__init__()
@@ -41,33 +51,36 @@ class MyWindow(QMainWindow):
         self.myDialog.show()
     def checkbox(self):
         self.hide()
-        self.cb = checkbox_Dialog()
-        self.cb.setupUi(QDialog())
+        self.cb = CheckBox()
         self.cb.show()
 
     def updateGraph(self, i):
         if i==0:
             pass
-        elif i==1:
-            self.hide()
-            self.myDialog = MyDialog()
-            self.myDialog.show()
-        elif i==2:
-            #app = QApplication(sys.argv)
-            #self.hide()
+        else:
+            year=2000+i-1
+            print("chose year: ",year)
             QDesktopServices.openUrl(QUrl('http://127.0.0.1:8080/'))
-
-            # web = QWebEngineView()
-            # web.resize(640,480)
-            # web.load(QUrl('https://www.google.com/'))
-            # web.show()
-            #sys.exit(app.exec_())
-        elif i==3:
-            pass
-        elif i==4:
-            pass
-        elif i==5:
-            pass
+        # elif i==1:
+        #     self.hide()
+        #     self.myDialog = MyDialog()
+        #     self.myDialog.show()
+        # elif i==2:
+        #     #app = QApplication(sys.argv)
+        #     #self.hide()
+        #     QDesktopServices.openUrl(QUrl('http://127.0.0.1:8080/'))
+        #
+        #     # web = QWebEngineView()
+        #     # web.resize(640,480)
+        #     # web.load(QUrl('https://www.google.com/'))
+        #     # web.show()
+        #     #sys.exit(app.exec_())
+        # elif i==3:
+        #     pass
+        # elif i==4:
+        #     pass
+        # elif i==5:
+        #     pass
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
--- windows.py
+++ windows.py
@@ -1,4 +1,7 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import *
+from PyQt5.QtCore import *
 import pandas as pd
 
 class Ui_MainWindow(object):
@@ -142,3 +145,79 @@ class Ui_Dialog(object):
         self.label_4.setText(_translate("Dialog2", "Evolvement of network since year 2000"))
         self.label.setText(_translate("Dialog2", "select year"))
         self.pushButton.setText(_translate("Dialog2", "Load graph"))
+
+
+class checkbox_Dialog(object):
+
+    def setupUi(self, Dialog):
+        self.facultyList = pd.read_excel("data/Faculty.xlsx")["Faculty"]
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(501, 350)
+        self.verticalLayout = QtWidgets.QVBoxLayout(Dialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.scrollArea = QtWidgets.QScrollArea(Dialog)
+        self.scrollArea.setMinimumSize(QtCore.QSize(200, 50))
+        self.scrollArea.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.scrollArea.setHorizontalScrollBarPolicy(QtCore.Qt.ScrollBarAsNeeded)
+        self.scrollArea.setSizeAdjustPolicy(QtWidgets.QAbstractScrollArea.AdjustIgnored)
+        self.scrollArea.setWidgetResizable(True)
+        self.scrollArea.setObjectName("scrollArea")
+        self.scrollAreaWidgetContents = QtWidgets.QWidget()
+        self.scrollAreaWidgetContents.setGeometry(QtCore.QRect(0, 0, 474, 348))
+        self.scrollAreaWidgetContents.setObjectName("scrollAreaWidgetContents")
+        self.gridLayout = QtWidgets.QGridLayout(self.scrollAreaWidgetContents)
+        self.gridLayout.setObjectName("gridLayout")
+        self.okButton = QtWidgets.QPushButton(Dialog)
+        self.okButton.setGeometry(QtCore.QRect(230, 40, 71, 31))
+        font = QtGui.QFont()
+        font.setPointSize(14)
+        self.okButton.setFont(font)
+        self.okButton.setObjectName("okButton")
+        self.okButton.clicked.connect(self.findState)
+        self.okButton.clicked.connect(Dialog.myWindow)
+        for i in range(1,18):
+            exec("""self.horizontalLayout_{} = QtWidgets.QHBoxLayout()
+self.horizontalLayout_{}.setObjectName("horizontalLayout_{}")
+self.checkbox_{} = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
+self.checkbox_{}.setObjectName("{}")
+self.horizontalLayout_{}.addWidget(self.checkbox_{})
+self.checkbox_{} = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
+self.checkbox_{}.setObjectName("{}")
+self.horizontalLayout_{}.addWidget(self.checkbox_{})
+self.checkbox_{} = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
+self.checkbox_{}.setObjectName("{}")
+self.horizontalLayout_{}.addWidget(self.checkbox_{})
+self.checkbox_{} = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
+self.checkbox_{}.setObjectName("{}")
+self.horizontalLayout_{}.addWidget(self.checkbox_{})
+self.checkbox_{} = QtWidgets.QCheckBox(self.scrollAreaWidgetContents)
+self.checkbox_{}.setObjectName("{}")
+self.horizontalLayout_{}.addWidget(self.checkbox_{})
+self.gridLayout.addLayout(self.horizontalLayout_{}, {}, 0, 1, 1)
+            """.format(i,i,i,(i-1)*5,(i-1)*5,self.facultyList[(i-1)*5],i,(i-1)*5,
+                       (i-1)*5+1,(i-1)*5+1,self.facultyList[(i-1)*5+1],i,(i-1)*5+1,
+                       (i-1)*5+2,(i-1)*5+2,self.facultyList[(i-1)*5+2],i,(i-1)*5+2,
+                       (i-1)*5+3,(i-1)*5+3,self.facultyList[(i-1)*5+3],i,(i-1)*5+3,
+                       (i - 1) * 5+4, (i - 1) * 5+4, self.facultyList[(i - 1) * 5+4], i, (i - 1) * 5+4,
+                       i, i-1))
+        self.scrollArea.setWidget(self.scrollAreaWidgetContents)
+        self.verticalLayout.addWidget(self.scrollArea)
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "Dialog"))
+        self.okButton.setText(_translate("Dialog", "ok"))
+        for i in range(85):
+            exec("""self.checkbox_{}.setText(_translate("Dialog", "{}"))""".format(i,self.facultyList[i]))
+
+    def findState(self):
+        ret = []
+        for i in range(85):
+            exec("""if self.checkbox_{}.isChecked():
+    ret.append(self.checkbox_{}.text())""".format(i, i))
+        print(ret)
+        QDesktopServices.openUrl(QUrl('http://127.0.0.1:8080/'))
+        return ret
